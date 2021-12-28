b0e9fca6aa095fa1a9e4db0982234cf27ea4d385
wassim93 <wassim.benhassen@esprit.tn>
2021-12-28 23:14:53 +0000

    fixing amount chnage issue

--- pix_image_finder/src/components/Search/Search.js
+++ pix_image_finder/src/components/Search/Search.js
@@ -6,7 +6,7 @@ import axios from "axios";
 export class Search extends Component {
   state = {
     searchText: "",
-    amout: 15,
+    amount: 15,
     apiUrl: "https://pixabay.com/api/",
     apiKey: "25015327-bbdd746e3de04d1e2bc552e25",
     images: [],
@@ -26,8 +26,8 @@ export class Search extends Component {
         <SelectField
           name="amount"
           floatingLabelText="amount"
-          value={this.state.amout}
           onChange={this.onAmountChange}
+          value={this.state.amount}
         >
           <MenuItem value={5} primaryText="5" />
           <MenuItem value={10} primaryText="10" />
@@ -44,17 +44,15 @@ export class Search extends Component {
     this.setState({ searchText: e.target.value }, () => {
       axios
         .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amout}&safesearch=true`
+          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
         )
         .then((res) => this.setState({ images: res.data.hits }))
         .catch((err) => console.log(err));
     });
     console.log("textchange");
   };
-  onAmountChange = (e) => {
-    /* const { amount } = this.state;
-    this.setState({ amount: e.target.value });
-    console.log("amountchange"); */
+  onAmountChange = (e, index, value) => {
+    this.setState({ amount: value });
   };
 }
 
